b942f7414a7e1129886b93b888b397beacc9d70d
Henry Winters <hlwinters17@gmail.com>
2022-09-18 23:36:42 -0700

    Added express and mongoose installs

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
 
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</>')
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
