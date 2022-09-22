68f6bfc087456a557b04a44c88f914854b0a17f1
Henry Winters <hlwinters17@gmail.com>
2022-09-21 21:52:57 -0700

    Added User model and controller

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const usersRouter = require('./controllers/users')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/users', usersRouter)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</>')
 })
 
-const PORT = 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
+
+module.exports = app 
---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: String, 
+    name: String, 
+    passwordHash: String, 
+    workouts: [
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Workout'
+        }
+    ]
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
